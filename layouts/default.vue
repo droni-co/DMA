76d0afd1ca9a5e5689ad2c1b1810439010c821bc
Gustavo Barragan <dev@droni.co>
2024-12-09 19:12:06 -0500

    Scaffold

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <h1>Hola mundo</h1>
+  </header>
+  <main>
+    <slot />
+  </main>
+  <footer>
+    <p>© 2024</p>
+  </footer>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h2>Bienvenido a DMA</h2>
+  </div>
+</template>
