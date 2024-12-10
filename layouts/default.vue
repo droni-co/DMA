2fc118f0282cf3452ab71194d9411c20bfaf345a
Gustavo Barragan <dev@droni.co>
2024-12-10 17:32:46 -0500

    dark mode

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,11 +3,14 @@
     bg-slate-200
     flex flex-col
     min-h-screen
-    bg-gradient-to-b
+    bg-gradient-to-br
     dark:from-slate-900
     dark:to-slate-950
     dark:text-slate-100">
     <main class="grow">
+      <UiFormButton @click="toggleColorMode" class="fixed top-4 right-4">
+        {{ colorMode.value === 'dark' ? '🌞' : '🌙'  }}
+      </UiFormButton>
       <slot />
     </main>
     <footer class="bg-slate-800 text-slate-100 py-4">
@@ -19,3 +22,11 @@
     </footer>
   </div>
 </template>
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+const toggleColorMode = () => {
+  colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+}
+
+</script>
